d7f600583024eb926ad82a88eeb5dbd2b16515f8
jaox <jaox@jaoxs-MacBook-Air.local>
2019-05-08 10:32:27 -0500

    usamos threads

--- dense_points/ParallelSamplingDensePoints.h
+++ dense_points/ParallelSamplingDensePoints.h
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <pthread.h>
+#include <thread>
 #include <vector>
 #include <list>
 #include <math.h>
@@ -16,7 +16,7 @@
 
 using namespace std;
 
-pthread_mutex_t mutex1;
+mutex mutex_;
 
 int numberPoints;
 double r = 5;
@@ -31,73 +31,51 @@ vector<vector<Sample*>> actives;//
 vector<vector<Sample*>> idles;  //
 vector<list<Sample*>> grid(cols*fils);   //inicializar el grid !!! esta sera la respuesta
 
-
-
-
-struct parallel_data {
-    int ini;
-    int fin;
-    int id_t;
-};
-
-void* parallel_generateDP(void* arg) {
-    
-    parallel_data* data1 = (parallel_data*) arg;
-    
-    for (int i = data1->ini; i < data1->fin; ++i) {
-        
+void parallel_generateDP(int ini, int fin) {
+    for (int i = ini; i < fin; ++i) {
         Sample* sample = new Sample();
         sample->pos[0] = rand()%(WIDTH-1);  //numeros aleatorios entre 0 y 500
         sample->pos[1] = rand()%(HEIGHT-1);
         sample->status = "IDLE";
       //  printf("x : %d y : %d\n", sample->pos[0], sample->pos[1]);
-        
-        pthread_mutex_lock(&mutex1);
+        mutex_.lock();
         cloud[i] = sample;
-        
         //agregar al grid cell correspondiente
         int ii = sample->pos[0]/size_;
         int j = sample->pos[1]/size_;
         int id_grid_cell = ii + j * cols;
        // printf("id : %d cell: %d \n",data1->id_t, id_grid_cell);
         grid[id_grid_cell].push_back(sample);
-        
         pointIndex[i] = i;
-        
-        pthread_mutex_unlock(&mutex1);
+        mutex_.unlock();
     }
-    
-    return NULL;
 };
 
 class ParallelSamplingDensePoints {
     
-    pthread_t hilos[N_THREADS];
+    thread hilos[N_THREADS];
     
 public:
     void generateDensePoints() {
-    
         for (int i = 0; i < N_THREADS; ++i) {
-            parallel_data* args = new parallel_data();
-            args->ini = i * (L/2);
-            args->fin = (i+1) * (L/2);
-            args->id_t = i;
-            pthread_create(&hilos[i], NULL, parallel_generateDP, (void*) args);
+            int ini = i * (L/2);
+            int fin = (i+1) * (L/2);
+            hilos[i] = thread(parallel_generateDP, ini, fin);
         }
         
         for(int i = 0; i < N_THREADS; ++i) {
-            pthread_join(hilos[i], NULL);
+            hilos[i].join();
         }
         
     //    printf("puntos en la nube : %lu\n",cloud.size());
     
-    
-    std::cout<<L<<'\n';
-    for(auto &i : grid){
-        for(auto &j : i){
-            std::cout<<j->pos[0]<<" "<<j->pos[1]<<std::endl;
+        //int count = 0;
+        std::cout<<L<<'\n';
+        for(auto i : grid){
+            for(auto j : i){
+                std::cout<<j->pos[0]<<" "<<j->pos[1]<<std::endl;
+                //++count;
+            }
         }
     }
-    }
-
 };
